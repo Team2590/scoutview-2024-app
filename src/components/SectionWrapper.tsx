8ccc32dcb6f2125a78cac17bacccecd7051777c6
dmanslick <dvvshah@gmail.com>
2024-04-17 23:23:58 -0400

    idk some uncommited change

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 
 export default function SectionWrapper({ children, label }: { children: React.ReactNode, label: string }) {
     return (
-        <section style={{ minHeight: window.innerHeight, position: 'relative' }}>
+        <section style={{ minHeight: 'max(100vh, 529px)', position: 'relative' }}>
             <Header label={label} />
             {children}
         </section>
