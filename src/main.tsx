527c09f73301895c674b2ca7e788160508add8bf
dmanslick <dvvshah@gmail.com>
2024-08-30 14:56:59 -0400

    Using only scss files now

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/style.css'
+import './styles/style.scss'
 // import './styles/debug.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
